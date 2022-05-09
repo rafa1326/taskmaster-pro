366904e77c070fe32b8eb0850f13ed0bf0a2e44a
rafa1326 <phillyaryeh@gmail.com>
2022-05-09 11:38:26 -0400

    mediaqueryui

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -120,10 +120,78 @@ $(".list-group").on("click", "p", function() {
   
     $(this).replaceWith(taskSpan);
   });
-   
+ 
+  $(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event) {
+      console.log("activate", this);
+    },
+    deactivate: function(event) {
+      console.log("deactivate", this);
+    },
+    over: function(event) {
+      console.log("over", event.target);
+    },
+    out: function(event) {
+      console.log("out", event.target);
+    },
+    update: function(event) {
+      var tempArr = [];
+     
+      $(this).children().each(function() {
+        
+       
+        var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+     
+        var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+    
+        tempArr.push({
+          text: text,
+          date: date
+        });
+      });
+       
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    
+      tasks[arrName] = tempArr;
+      saveTasks();
+     
+      console.log(tempArr);
+      
+    }
+  
+   });
+
+   $("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+      console.log("drop");
+      ui.draggable.remove();
+    },
+    over: function(event, ui) {
+      console.log("over");
+    },
+    out: function(event, ui) {
+      console.log("out");
+    }
+  });
 
 
-// modal was triggered
+
+
+   // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
