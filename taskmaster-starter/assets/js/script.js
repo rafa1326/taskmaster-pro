00b0fb88e28bbe9c4855135e3fca535e10ed0ff4
rafa1326 <phillyaryeh@gmail.com>
2022-05-09 21:03:54 -0400

    UX enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -138,17 +138,29 @@ $(".list-group").on("click", "p", function() {
     scroll: false,
     tolerance: "pointer",
     helper: "clone",
+   
     activate: function(event) {
       console.log("activate", this);
+      
+     $(this).addClass("dropover");
+    
+     $(".bottom-trash").addClass("bottom-trash-bag");
     },
+
     deactivate: function(event) {
       console.log("deactivate", this);
+      $(this).removeClass("dropover");
+     
+     $(".bottom-trash").removeClass(bottom-trash-drag);
     },
     over: function(event) {
       console.log("over", event.target);
+      $(event.target).addClass("dropover-active");
+   
     },
     out: function(event) {
       console.log("out", event.target);
+     $(event.Target).removeClass("dropover-active");
     },
     update: function(event) {
       var tempArr = [];
@@ -191,12 +203,16 @@ $(".list-group").on("click", "p", function() {
     drop: function(event, ui) {
       console.log("drop");
       ui.draggable.remove();
+      $(".bottom-trash").removeClass("bottom-trash-active");
     },
     over: function(event, ui) {
       console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event, ui) {
       console.log("out");
+   
+    $(".bottom-trash").removeClass("bottom-trash-active");
     }
   });
 
@@ -214,6 +230,7 @@ $(".list-group").on("click", "p", function() {
     else if (Math.abs(moment().diff(time, "days")) <= 2) {
       $(taskEl).addClass("list-group-item-warning");
     }
+    console.log(taskEl)
   };
 
 
@@ -231,7 +248,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -267,3 +284,8 @@ loadTasks();
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
